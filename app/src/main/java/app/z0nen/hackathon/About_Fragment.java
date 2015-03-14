c935f819323f0119bf83a608a9a13d99e4705752
Rivka Garcia <beccagarcia11@gmail.com>
2015-03-14 15:06:54 -0700

    Font changes

--- app/src/main/java/app/z0nen/hackathon/About_Fragment.java
+++ app/src/main/java/app/z0nen/hackathon/About_Fragment.java
@@ -1,12 +1,16 @@
 package app.z0nen.hackathon;
 
 import android.app.Fragment;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.TextView;
+import android.content.Context;
 
 /**
  * Created by Devon on 3/14/2015.
@@ -15,8 +19,8 @@ public class About_Fragment extends Fragment {
     View rootview;
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         rootview = inflater.inflate(R.layout.about_layout, container, false);
 
@@ -27,4 +31,15 @@ public class About_Fragment extends Fragment {
 //        rootview = inflater.inflate(R.layout.about_layout, container, false);
         return rootview;
     }
+    public void fontClass(Context myContext) {
+        TextView title;
+        Typeface custom_font = Typeface.createFromAsset(myContext.getAssets(), "fonts/Asap-Regular.otf");
+
+
+        title = (TextView) rootview.findViewById(R.id.textView);
+        title.setTypeface(custom_font);
+
+    }
+
+
 }
