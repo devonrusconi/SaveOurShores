767dc34fc7619334bd63a39e8f30616c10319e3f
Devon Rusconi <drusconi@csumb.edu>
2015-03-14 14:41:15 -0700

    about us

--- app/src/main/java/app/z0nen/hackathon/About_Fragment.java
+++ app/src/main/java/app/z0nen/hackathon/About_Fragment.java
@@ -18,13 +18,13 @@ public class About_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-        rootview = inflater.inflate(R.layout.about_layout, container, false);
+
 
 //        String url = "<html><body>Youtube video .. <br> <iframe class=\"youtube-player\" type=\"text/html\" width=\"640\" height=\"385\" src=\"http://www.youtube.com/embed/bIPcobKMB94\" frameborder=\"0\"></body></html>";
 //        WebView myWebView = (WebView) rootview.findViewById(R.id.webView);
 //        myWebView.loadData(url, "text/html", "utf-8");
-//
-//        rootview = inflater.inflate(R.layout.about_layout, container, false);
+
+        rootview = inflater.inflate(R.layout.about_layout, container, false);
         return rootview;
     }
 }
