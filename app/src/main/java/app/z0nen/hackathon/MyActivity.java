0c92986c226d777da18e2cb48c47059de664b5ba
Devon Rusconi <drusconi@csumb.edu>
2015-03-14 23:36:24 -0700

    deleted files

--- app/src/main/java/app/z0nen/hackathon/MyActivity.java
+++ app/src/main/java/app/z0nen/hackathon/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
@@ -11,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MyActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
