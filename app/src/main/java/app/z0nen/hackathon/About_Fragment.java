158135d62412dcc52ecaf6a5c11eef6b81f11d55
Rivka Garcia <beccagarcia11@gmail.com>
2015-03-14 17:09:40 -0700

    removed images

--- app/src/main/java/app/z0nen/hackathon/About_Fragment.java
+++ app/src/main/java/app/z0nen/hackathon/About_Fragment.java
@@ -1,14 +1,12 @@
 package app.z0nen.hackathon;
 
 import android.app.Fragment;
-import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.TextView;
 import android.content.Context;
 
@@ -33,7 +31,7 @@ public class About_Fragment extends Fragment {
     }
     public void fontClass(Context myContext) {
         TextView title;
-        Typeface custom_font = Typeface.createFromAsset(myContext.getAssets(), "fonts/Asap-Regular.otf");
+        Typeface custom_font = Typeface.createFromAsset(myContext.getAssets(), "src/main/Asap-Regular.otf");
 
 
         title = (TextView) rootview.findViewById(R.id.textView);
