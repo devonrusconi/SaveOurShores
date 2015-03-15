4510a120824ac4df566006f85bca454821da927d
Devon Rusconi <drusconi@csumb.edu>
2015-03-15 00:29:48 -0700

    hey

--- app/src/main/java/app/z0nen/hackathon/Contact_Fragment.java
+++ app/src/main/java/app/z0nen/hackathon/Contact_Fragment.java
@@ -6,8 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
+//import com.google.android.gms.maps.*;
+//import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.os.Bundle;
 /**
@@ -22,30 +22,29 @@ public class Contact_Fragment extends Fragment {
         rootview = inflater.inflate(R.layout.contact_layout, container, false);
         return rootview;
     }
-    public class MapPane extends Activity implements OnMapReadyCallback {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.map_activity);
-
-            MapFragment mapFragment = (MapFragment) getFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        }
-
-        @Override
-        public void onMapReady(GoogleMap map) {
-            LatLng sydney = new LatLng(-33.867, 151.206);
-
-            map.setMyLocationEnabled(true);
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-
-            map.addMarker(new MarkerOptions()
-                    .title("Sydney")
-                    .snippet("The most populous city in Australia.")
-                    .position(sydney));
-        }
+//    public class MapPane extends Activity implements OnMapReadyCallback {
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.map_activity);
+//
+//            MapFragment mapFragment = (MapFragment) getFragmentManager()
+//                    .findFragmentById(R.id.map);
+//            mapFragment.getMapAsync(this);
+//        }
+//
+//        @Override
+//        public void onMapReady(GoogleMap map) {
+//            LatLng sydney = new LatLng(-33.867, 151.206);
+//
+//            map.setMyLocationEnabled(true);
+//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+//
+//            map.addMarker(new MarkerOptions()
+//                    .title("Sydney")
+//                    .snippet("The most populous city in Australia.")
+//                    .position(sydney));
+//        }
     }
 
-}
